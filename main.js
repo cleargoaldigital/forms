43c6dd3da1bff210099f16abd59eb0925f02f641
Taofeek Ajibade <81028891+cleargoaldigital@users.noreply.github.com>
2022-01-26 19:51:45 +0100

    Added files

---
+++ main.js
@@ -0,0 +1,37 @@
+console.log('Hello World!');
+
+function Bird() {
+  let weight = 15;
+  this.getWeight = function() {
+    return weight;
+  }
+}
+let duck = new Bird();
+duck.getWeight();
+console.log(Bird());
+
+
+const eventColor = document.querySelector('button');
+const body = document.querySelector('body');
+document.body.style.backgroundColor = 'green';
+document.body.style.color = "#fed";
+eventColor.textContent = "Change text and background colour";
+
+eventColor.addEventListener('click', () => {
+    if(document.body.style.backgroundColor === 'green'){
+      document.body.style.backgroundColor = 'orange';
+      document.body.style.color = 'skyblue';
+      eventColor.style.backgroundColor = "blue";
+    }
+  else if(document.body.style.backgroundColor=== "orange"){
+    document.body.style.backgroundColor = "red";
+    document.body.style.color = "#feddef";
+    eventColor.textContent = "Akanbi";
+  }
+  else {
+    document.body.style.backgroundColor = "green";
+    document.body.style.color = "#fff";
+    eventColor.textContent = "Abdulhayy";
+    eventColor.style.backgroundColor = "red";
+  }
+});   
