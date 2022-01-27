7dede22f2bb3237f07ac6a49149a10378c4238f8
cleargoaldigital <cleargoal.ng@gmail.com>
2022-01-27 16:17:59 +0100

    updated css file

--- main.js
+++ main.js
@@ -129,37 +129,24 @@ function convertToRoman(num) {
   // Ceasar Cipher
 
   const alphabets = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+    "S", "T", "U", "V", "W", "X", "Y", "Z",
   ];
 
 function rot13(str) {
- 
-  return str;
+ let resulti = "";
+ for(let i = 0; i < str.length; i++){
+ const char = str[i];
+ const isALetter = alphabets.includes(char);
+ if (isALetter === false){
+   resulti += char;
+ } else{
+   const charIndex = alphabets.findIndex((e) => e === char);
+   resulti += alphabets[charIndex + 13] || alphabets[charIndex - 13];
+ }
+  
 }
 
-rot13("SERR PBQR PNZC");
+return resulti;
+}
+console.log(rot13("SERR PBQR PNZC"));
