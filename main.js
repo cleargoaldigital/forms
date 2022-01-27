b02b5c86d57c16dd719b1f1f0af1c0d373c96b9a
cleargoaldigital <cleargoal.ng@gmail.com>
2022-01-27 09:24:19 +0100

    added codes - roman numerals

--- main.js
+++ main.js
@@ -45,3 +45,83 @@ submit.addEventListener('click', ()=>{
 reset.addEventListener('click', ()=>{
   confirm("Are you sure to reset the form?")
 })
+
+function convertToRoman(num) {
+  let result = "";
+  
+  while (num >= 1000){
+      result += "M";
+      num -= 1000;
+    }
+  
+  while (num >= 900){
+      result += "CM";
+      num -= 900;
+    }
+  
+  while (num >= 500){
+      result += "D";
+      num -= 500;
+    }
+  
+  while (num >= 400){
+      result += "CD";
+      num -= 400;
+    }
+  
+  while (num >= 100){
+      result += "C";
+      num -= 100;
+    }
+  
+  while (num >= 90){
+      result += "XC";
+      num -= 90;
+    }
+  
+  while (num >= 50){
+      result += "L";
+      num -= 50;
+    }
+  
+  while (num >= 40){
+      result += "XL";
+      num -= 40;
+    }
+  
+  while (num >= 10){
+      result += "X";
+      num -= 10;
+    }
+  
+  while (num === 9){
+      result += "IX";
+      num -= 9;
+    }
+  
+  while (num >= 5){
+      result += "V";
+      num -= 5;
+    }
+  
+  while (num >= 4){
+      result += "IV";
+      num -= 4;
+    }
+  while (num === 3){
+      result += "III";
+      num -= 3;
+    }
+   while (num === 2){
+      result += "II";
+      num -= 2;
+    }
+    while (num === 1){
+      result += "I";
+      num -= 1;
+    }
+   return result;
+  }
+  
+  
+  console.log(convertToRoman(1999));
